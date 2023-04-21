8b531d2759feb0903d999fb12faf042dadd04ed4
kuchina-anastasia11 <112166202+kuchina-anastasia11@users.noreply.github.com>
2023-04-21 17:55:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,9 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x = np.sort(x)
+    med = np.median(x)
+    d =float(9*(med**2) -6*med + 1 + 0.24)
+    d = np.sqrt(d)
+    lam = (3*med - 1 + d) / 264
+    return lam # Ваш отве
